4e3c772f60676f0dd5c7753b86138f818ea27829
Philippe Luchansky <philuchansky@gmail.com>
2015-06-30 12:47:57 -0700

    notes

--- js/app.js
+++ js/app.js
@@ -9,13 +9,22 @@ var btnPlus = document.querySelector('#btn-plus');
 //btn-equals
 var btnEquals = document.querySelector('#btn-equals');
 
+var currentOperation;
+
 //button functions
 function input(){
     readout.innerHTML += this.innerHTML;
+    currentOperation += this.val;
+    //console.log('The value of this is ' + this.val);
 }
 
 function reset() {
     readout.innerHTML = "";
+}
+
+//addition function
+function addThis(firstVal,secondVal){
+    //use this function as the 2nd index of a 3-index array, where the 1st and 2nd indeces are the values being passed to this function.
 }
 
 function equals() {
@@ -25,6 +34,7 @@ function equals() {
 
 for(i = 0; i < numButtons.length; i ++) {
     numButtons[i].addEventListener('click',input);
+    numButtons[i].val = i;
 }
 
 
